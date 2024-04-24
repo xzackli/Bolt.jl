1613d5c44ff04ebef03abb738f95f84289fa9832
Jamie Sullivan <jmsullivan@berkeley.edu>
2024-04-24 15:54:27 -0700

    cmb cl transfer

--- examples/plot_deriv_cl.jl
+++ examples/plot_deriv_cl.jl
@@ -1,9 +1,96 @@
 using Bolt
 using ForwardDiff
-# using PyPlot
 using Plots
 using BenchmarkTools
+using Plots.PlotMeasures
 
+
+# General derivates of  all other parameters
+function clp(𝕡::DT,ells) where DT
+    bg = Background(𝕡; x_grid=-20.0:0.01:0.0, nq=15)
+    𝕣 = Bolt.RECFAST(bg=bg, Yp=𝕡.Y_p, OmegaB=𝕡.Ω_b, OmegaG=𝕡.Ω_r)
+    k_grid = quadratic_k(0.1bg.H₀, 1000bg.H₀, 100)
+    ih = IonizationHistory(𝕣, 𝕡, bg)
+    sf = source_grid(𝕡, bg, ih, k_grid, BasicNewtonian())
+    return cltt(ells, 𝕡, bg, ih, sf)
+end
+
+
+pt = CosmoParams() #dummy struct
+ells = 10:20:1200
+# do a loop over everyone to see who is slow...this is bad for performance but who cares we are just checking times inside...
+fds = zeros(length(fieldnames(typeof(pt))),length(ells))
+fws = zeros(length(fieldnames(typeof(pt))),length(ells))
+
+for (i,name) in enumerate(fieldnames(typeof(pt)))
+    println("i = ", i, " $name = ",getfield(pt,name))
+    xv = getfield(pt,name)#pt.name
+    function clx(x::DT,name,ells) where DT
+        𝕡n = CosmoParams{DT}(;Dict((fieldnames(typeof(pt))[i])=>x)...)
+        return clp(𝕡n,ells)
+    end
+    f(x) = clx(x,name,ells)
+    fwddiff_∂cl = ForwardDiff.derivative(f,xv)
+    println(fwddiff_∂cl[1])
+    Δ = 1e-2*xv 
+    finitediff_∂cl = (f(xv + Δ) .- f(xv - Δ)) ./ 2Δ
+    fws[i,:] = fwddiff_∂cl
+    fds[i,:] = finitediff_∂cl
+end
+
+#multiply the mnu by its value to get something of OOM as the others...
+#just an ugliness of the Mpc units for the eV neutrino mass...
+fws[end,:] .*= 𝕡.Σm_ν
+fds[end,:] .*= 𝕡.Σm_ν
+
+# save the results
+writedlm("ClTT_AD.dat",fws)
+writedlm("ClTT_FD1e-2.dat",fds)
+
+#make some plots
+pnames = string.( fieldnames(typeof(pt)) )
+for i in 1:length(fieldnames(typeof(pt)))
+    plot(title=raw"$ p = $"*pnames[i])
+    plot!(ells, fws[i,:] .* ells.^2,label="AD")
+    plot!(ells, fds[i,:] .* ells.^2,ls=:dash,label="FD, "*raw"$\frac{\Delta p}{p} = 10^{-2}$")
+    xlabel!(raw"$\ell$")
+    ylabel!(raw"$\ell^{2} \frac{\partial C_{\ell}^{TT}}{\partial p }$",left_margin=5mm)
+    savefig("AD_vs_FD1e-2_all_params_"*pnames[i]*".pdf")
+end
+
+
+
+#-----FIXME move the below, which just tests scalar polzn spectra, and has nothing to do with derivs
+
+𝕡 = CosmoParams()
+bg = Background(𝕡; x_grid=-20.0:0.01:0.0, nq=15)
+𝕣 = Bolt.RECFAST(bg=bg, Yp=𝕡.Y_p, OmegaB=𝕡.Ω_b, OmegaG=𝕡.Ω_r)
+ih = IonizationHistory(𝕣, 𝕡, bg)
+k_grid = quadratic_k(0.1bg.H₀, 1000bg.H₀, 100)
+sf_t = source_grid(𝕡, bg, ih, k_grid, BasicNewtonian())
+sf_e = source_grid_P(𝕡, bg, ih, k_grid, BasicNewtonian())
+
+ℓs = 10:20:1200
+Cℓtt = cltt(ℓs, 𝕡, bg, ih, sf_t)
+Cℓte = clte(ℓs, 𝕡, bg, ih, sf_t,sf_e)
+Cℓee = clee(ℓs, 𝕡, bg, ih, sf_e)
+
+ℓfac = ℓs.*(ℓs.+1)
+plot(ℓs, @. ( ℓfac * Cℓtt))
+xlabel!(raw"$\ell$")
+ylabel!(raw"$\ell(\ell+1)C^{TT}_{\ell}$")
+# savefig("./test/cltt.png")
+plot(ℓs, @. (ℓfac *Cℓte))
+xlabel!(raw"$\ell$")
+ylabel!(raw"$\ell(\ell+1)C^{TE}_{\ell}$")
+# savefig("./test/clte.png")
+plot(ℓs, @. (ℓfac * Cℓee))
+xlabel!(raw"$\ell$")
+ylabel!(raw"$\ell(\ell+1)C^{EE}_{\ell}$")
+# savefig("./test/clee.png")
+
+
+#--- old code
 # Cₗ as a function of baryon density
 #Some noise at lowest few ells...9/17/21
 
@@ -22,20 +109,18 @@ f(Ω_b) = clb(Ω_b, ells)
 @time cl = f(0.046)
 @time ∂cl = ForwardDiff.derivative(f, 0.046)  # you can just ForwardDiff the whole thing
 
-##
-# clf()
-# plt.figure(figsize=(10,5))
-plot(ells, cl .* ells.^2, label=raw"$C_{\ell}$",lw=0,marker=:circle)
-plot!(ells, ∂cl .* ells.^2 / 10,
-    label=raw"$\partial C_{\ell}/\partial\Omega_b / 10$",lw=0,marker=:circle)
+@profview f(0.046)
+#58.130749 seconds (74.01 M allocations: 7.458 GiB, 3.10% gc time, 2.92% compilation time)
+@profview ForwardDiff.derivative(f, 0.046)
+#219.854089 seconds (139.67 M allocations: 13.256 GiB, 1.45% gc time, 8.75% compilation time)
 
-# uncomment to see finite differences
-# Δ = 1e-3
-@time finitediff_∂cl = (f(0.046 + Δ) .- f(0.046 - Δ)) ./ 2Δ
-plot!(ells, finitediff_∂cl .* ells.^2 / 10, ls=:dash,
-    label=raw"$\Delta C_{\ell}/\Delta\Omega_b / 10$")
 
-ylabel!(raw"$\ell^2 C_{\ell}^{TT}$")
-xlabel!(raw"$\ell$")
-ylims!(-0.3, 0.5)
-# savefig("docs/assets/example_spectrum.png")
+ells = 10:20:1200
+fh(h) = clh(h, ells)
+@time clhh = fh(0.7)
+#^ 59.244405 seconds (72.37 M allocations: 7.376 GiB, 3.04% gc time, 0.28% compilation time)
+@time ∂clhh = ForwardDiff.derivative(fh, 0.7)  # you can just ForwardDiff the whole thing
+#^ 217.447492 seconds (109.88 M allocations: 12.040 GiB, 1.53% gc time, 9.47% compilation time)
+
+@profview fh(0.7)
+@profview ForwardDiff.derivative(fh, 0.7)
--- src/spectra.jl
+++ src/spectra.jl
@@ -21,6 +21,25 @@ function source_grid(par::AbstractCosmoParams{T}, bg, ih, k_grid,
     return itp
 end
 
+function source_grid_P(par::AbstractCosmoParams{T}, bg, ih, k_grid,
+    integrator::PerturbationIntegrator; ℓᵧ=8, reltol=1e-11) where T
+    x_grid = bg.x_grid
+    grid = zeros(T, length(x_grid), length(k_grid))
+    @qthreads for (i_k, k) in enumerate(k_grid)
+        hierarchy = Hierarchy(BasicNewtonian(), par, bg, ih, k, ℓᵧ)
+        perturb = boltsolve(hierarchy; reltol=reltol)
+        for (i_x, x) in enumerate(x_grid)
+            u = perturb(x)  # this can be optimized away, save timesteps at the grid!
+            du = similar(u)
+            Bolt.hierarchy!(du, u, hierarchy, x)
+            grid[i_x,i_k] = Bolt.source_function_P(du, u, hierarchy, x)
+        end
+    end
+    # return grid
+    itp = LinearInterpolation((x_grid, k_grid), grid, extrapolation_bc = Line())
+    return itp
+end
+
 # we make the assumption that shifting the coordinates upon which we integrate
 # does not affect our result. that is, we choose coordinates where the integral converges
 assume_nondual(x::ForwardDiff.Dual) = ForwardDiff.value(x)
@@ -59,6 +78,19 @@ function Θl(x_i, k, s_itp, bes, par::AbstractCosmoParams{T}, bg) where {T}
     return s
 end
 
+# For polzn we have moved the factor of (bg.η₀ - bg.η(x))^-2 from S_p to here
+function Pl(x_i, k, s_itp, bes, par::AbstractCosmoParams{T}, bg) where {T}
+    s = zero(T)
+    xgrid = bg.x_grid
+    for i in x_i:length(xgrid)-1
+        x = xgrid[i]
+        sb = bes(k*(bg.η₀ - bg.η(x)))
+        source = s_itp(x, k)
+        s += sb * source * (xgrid[i+1] - xgrid[i])
+    end
+    return s
+end
+
 function cltt(ℓ, s_itp, kgrid, par::AbstractCosmoParams{T}, bg) where {T}
     bes = Bolt.bessel_interpolator(ℓ, kgrid[end] * bg.η₀)
     x_i = findfirst(bg.x_grid .> -8)  # start integrating after recombination
@@ -67,25 +99,82 @@ function cltt(ℓ, s_itp, kgrid, par::AbstractCosmoParams{T}, bg) where {T}
         k = kgrid[i]
         dk = kgrid[i+1] - kgrid[i]
         th = Θl(x_i, k, s_itp, bes, par, bg)
-        s += th^2 * dk / k
+        k_hMpc=k/(bg.H₀*2.99792e5/100) #This is messy...
+        Pprim = par.A*(k_hMpc/0.05)^(par.n-1)
+        s += th^2 * Pprim * dk / k
+    end
+    return s
+end
+
+#jms 6/7/22 UNTESTED
+function clte(ℓ, s_itp_t, s_itp_e, kgrid, par::AbstractCosmoParams{T}, bg) where {T}
+    bes = Bolt.bessel_interpolator(ℓ, kgrid[end] * bg.η₀)
+    ℓð = sqrt((ℓ+2)*(ℓ+1)*ℓ*(ℓ-1)) #spin factor
+    x_i = findfirst(bg.x_grid .> -8)  # start integrating after recombination
+    s = zero(T)
+    for i in 1:length(kgrid)-1
+        k = kgrid[i]
+        dk = kgrid[i+1] - kgrid[i]
+        th = Θl(x_i, k, s_itp_t, bes, par, bg)
+        ep = Pl(x_i, k, s_itp_e, bes, par, bg) * ℓð
+        k_hMpc=k/(bg.H₀*2.99792e5/100) #This is messy...
+        Pprim = par.A*(k_hMpc/0.05)^(par.n-1)
+        s += th * ep * Pprim * dk / k
+    end
+    return s
+end
+
+function clee(ℓ, s_itp_p, kgrid, par::AbstractCosmoParams{T}, bg) where {T}
+    bes = Bolt.bessel_interpolator(ℓ, kgrid[end] * bg.η₀)
+    ℓð = sqrt((ℓ+2)*(ℓ+1)*ℓ*(ℓ-1)) #spin factor
+    x_i = findfirst(bg.x_grid .> -8)  # start integrating after recombination
+    s = zero(T)
+    for i in 1:length(kgrid)-1
+        k = kgrid[i]
+        dk = kgrid[i+1] - kgrid[i]
+        ep = Pl(x_i, k, s_itp_p, bes, par, bg) * ℓð
+        k_hMpc=k/(bg.H₀*2.99792e5/100) #This is messy...
+        Pprim = par.A*(k_hMpc/0.05)^(par.n-1)
+        s += ep^2 * Pprim *  dk / k
     end
     return s
 end
 
 function cltt(ℓ::Int, par::AbstractCosmoParams, bg, ih, sf)
-    dense_kgrid = quadratic_k(0.1bg.H₀, 1000bg.H₀, 5000)
+    dense_kgrid = quadratic_k(0.01bg.H₀, 1000bg.H₀, 5000)
     cltt(ℓ, sf, dense_kgrid, par, bg)
 end
 
+function clte(ℓ::Int, par::AbstractCosmoParams, bg, ih, sf,sf_P)
+    dense_kgrid = quadratic_k(0.01bg.H₀, 1000bg.H₀, 5000)
+    clte(ℓ, sf, sf_P, dense_kgrid, par, bg)
+end
+
+function clee(ℓ::Int, par::AbstractCosmoParams, bg, ih,  sf_P)
+    dense_kgrid = quadratic_k(0.01bg.H₀, 1000bg.H₀, 5000)
+    clee(ℓ, sf_P, dense_kgrid, par, bg)
+end
+
 function cltt(ℓ⃗, par::AbstractCosmoParams, bg, ih, sf)
-    dense_kgrid = quadratic_k(0.1bg.H₀, 1000bg.H₀, 5000)
+    # dense_kgrid = quadratic_k(0.1bg.H₀, 1000bg.H₀, 5000) #what is the purpose of this?
     return qmap(ℓ->cltt(ℓ, par, bg, ih, sf), ℓ⃗)
+end
+
+function clte(ℓ⃗, par::AbstractCosmoParams, bg, ih, sf,sf_P)
+    # dense_kgrid = quadratic_k(0.1bg.H₀, 1000bg.H₀, 5000)
+    return qmap(ℓ->clte(ℓ, par, bg, ih, sf,sf_P), ℓ⃗)
+end
+
+function clee(ℓ⃗, par::AbstractCosmoParams, bg, ih, sf_P)
+    # dense_kgrid = quadratic_k(0.1bg.H₀, 1000bg.H₀, 5000)
+    return qmap(ℓ->clee(ℓ, par, bg, ih, sf_P), ℓ⃗)
 end
 
 
 function plin(k, 𝕡::AbstractCosmoParams{T},bg,ih,
-    n_q=15,ℓᵧ=50,ℓ_ν=50,ℓ_mν=20,x=0,reltol=1e-5) where T
-    #copy code abvoe
+              n_q=15,ℓᵧ=50,ℓ_ν=50,ℓ_mν=20,x=0,reltol=1e-5) where T
+
+    #copy code above
     hierarchy = Hierarchy(BasicNewtonian(), 𝕡, bg, ih, k, ℓᵧ,ℓ_ν,ℓ_mν,n_q) #shoddy quality test values
     perturb = boltsolve(hierarchy; reltol=reltol)
     results = perturb(x)
